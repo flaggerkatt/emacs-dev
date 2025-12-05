d5299776cc6024cc899c6799d60d994b069799fb
flaggerkatt <flaggerkatt@gmail.com>
2025-12-05 08:11:41 +0100

    new file:   testing.el

---
+++ testing.el
@@ -0,0 +1,25 @@
+;;; testing.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 per-karsten.nordhaug
+;;
+;; Author: per-karsten.nordhaug <per-karsten.nordhaug@AMAJLJ9M70YCW>
+;; Maintainer: per-karsten.nordhaug <per-karsten.nordhaug@AMAJLJ9M70YCW>
+;; Created: December 05, 2025
+;; Modified: December 05, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc
+;; Homepage: https://github.com/per-karsten.nordhaug/testing
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'testing)
+;;; testing.el ends here
